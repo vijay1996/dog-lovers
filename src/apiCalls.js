a39a93943c3cf1c702fa58bb745732a8908a1549
vijay1996 <vijaybhojraj.cm@gmail.com>
2021-10-02 08:01:36 +0530

    Created with intital setup

---
+++ src/apiCalls.js
@@ -0,0 +1,23 @@
+const URL = "https://api.thedogapi.com/v1/images/search?";
+
+export const fetchDogsFromApi = async ({
+  apiURL = URL,
+  limit = 10,
+  page = 10,
+  order = "Desc",
+}) => {
+  try {
+    const response = await fetch(
+      `${apiURL}limit=${limit}&page=${page}&order=${order}}`,
+      {
+        method: "GET",
+        headers: {
+          "X-Api-Key": `${process.env.REACT_APP_DOG_API_KEY}`,
+        },
+      }
+    );
+    return response.json();
+  } catch (error) {
+    return error;
+  }
+};
